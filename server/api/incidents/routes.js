11db2dfe12b15a731b5b3d047e72ab27c89eb88b
pratik.prasad <pratik.prasad@accenture.com>
2018-09-02 12:32:31 -0400

    Created memory Based Incidents API

--- server/api/incidents/routes.js
+++ server/api/incidents/routes.js
@@ -1,12 +1,40 @@
 const express = require('express')
 const router = express.Router({mergeParams: true})
 
-router.route('/incidentInfo')
+let incidents = [];
+
+router.route('/incidentInfo/:id')
   .get((req, res, next) => {
-    res.send(JSON.stringify({"type":"A","date":"02-02-2010"}))
+    res.send(JSON.stringify(getIncidentInfo(req.params.id)))
   })
   .post((req, res, next) => {
-    res.send('data saved')
+    res.send(JSON.stringify({result : saveIncidentInfo(req.body)}))
   })
+
+  let getIncidentInfo = (id) => {
+    console.log('Returning Incident #', id)
+    return incidents.find( x => x.id === id )
+  }
+  
+  let saveIncidentInfo = (data) => {
+    let incident = '';
+    if(data.id !== ''){
+      incident = incidents.find( x => x.id === data.id );
+    }else{
+      incident = {};
+    }
+    
+    incident.type = data.type
+    incident.driver = data.driver
+    incident.responsible = data.responsible
+    incident.when = data.when
+    
+    if(data.id === '') {
+      incident.id = incidents.length + 1
+      incidents.push(incident)
+    }
+  
+    return incidents.length;
+  }
 
 module.exports = router;
